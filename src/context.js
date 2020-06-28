8703c2efa33788bf07b5a922d91fa3a81da69faa
Sandhya Yadav <yadavshndhya@gmail.com>
2020-06-28 20:24:46 +0530

    product ui

--- src/context.js
+++ src/context.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import {storeProducts, detailProduct} from './data'; 
 const ProductContext = React.createContext();
 //provider
 //consumer
 
  class ProductProvider extends Component {
+     state = {
+         products:storeProducts,
+         detailProducts:detailProduct
+     }
+     handleDetail = () => {
+         console.log("hello from detail");
+     }
+     addToCart = () => {
+        console.log("hello from add to cart");
+    }
     render(){
         return (
-            <ProductContext.Provider value="hello from phone app">
+            <ProductContext.Provider value={{
+                ...this.state,
+                handleDetail:this.handleDetail,
+                addToCart:this.addToCart,
+            }}>
                 {this.props.children}
             </ProductContext.Provider>
             
