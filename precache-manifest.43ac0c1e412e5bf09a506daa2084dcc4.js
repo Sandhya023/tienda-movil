bed25d9fe5614c8205158e016968d08e6796e37b
Sandhya Yadav <yadavshndhya@gmail.com>
2020-06-29 18:19:42 +0530

    Updates

---
+++ precache-manifest.43ac0c1e412e5bf09a506daa2084dcc4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d381bc292e0dd3f73d00bf655feca66",
+    "url": "/teinda-movil/index.html"
+  },
+  {
+    "revision": "c39a48e46df1ee7de69a",
+    "url": "/teinda-movil/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "228ead1e4da5ec2c5444",
+    "url": "/teinda-movil/static/css/main.9902a1e2.chunk.css"
+  },
+  {
+    "revision": "c39a48e46df1ee7de69a",
+    "url": "/teinda-movil/static/js/2.6a5c6fdd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/teinda-movil/static/js/2.6a5c6fdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "228ead1e4da5ec2c5444",
+    "url": "/teinda-movil/static/js/main.19069bb2.chunk.js"
+  },
+  {
+    "revision": "7a78f0f86d5628140c7d",
+    "url": "/teinda-movil/static/js/runtime-main.e5e2e26f.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/teinda-movil/static/media/logo.9f7e506d.svg"
+  }
+]);
